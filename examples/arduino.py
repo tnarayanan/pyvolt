f24b33ca0950ff27121017821b3acdc302879a57
Tejas Narayanan <tejasn100@gmail.com>
2024-05-15 19:17:19 -0700

    Initial commit

---
+++ examples/arduino.py
@@ -0,0 +1,51 @@
+import pyvolt as pv
+from pyvolt import components as comp
+
+
+class Arduino(pv.Component):
+    def __init__(self, n_pins=5):
+        self.n_pins = n_pins
+        self.pin_voltages = [0 for _ in range(n_pins)]
+        self.pin_connections = [None for _ in range(n_pins)]
+
+        self.gnd_connection = None
+
+    def pin(self, pin):
+        return self.pin_connections[pin]
+
+    def gnd(self):
+        return self.gnd_connection
+
+    def pin_on(self, pin):
+        self.pin_voltages[pin] = 5
+
+    def pin_off(self, pin):
+        self.pin_voltages[pin] = 0
+
+
+circuit = pv.Circuit()
+
+# define the components of the circuit
+arduino = Arduino() in circuit
+resistor = comp.Resistor(ohm=150) in circuit
+diode = comp.Diode(v_f=2, i_f=20e-3) in circuit
+
+# define the connections between components
+arduino.pin(0) >> resistor.n1
+resistor.n2 >> diode.anode
+diode.cathode >> arduino.gnd()
+
+# set the circuit reference voltage
+circuit.gnd >> arduino.gnd()
+
+# CASE 1: pin 0 is on
+arduino.pin_on(0)
+circuit.compile()
+print("V before diode:", circuit.inspect_voltage(diode.anode))
+print("V after diode:", circuit.inspect_voltage(diode.cathode))
+
+# CASE 2: pin 0 is off
+arduino.pin_off(0)
+circuit.compile()
+print("V before diode:", circuit.inspect_voltage(diode.anode))
+print("V after diode:", circuit.inspect_voltage(diode.cathode))
---
+++ examples/led.py
@@ -0,0 +1,19 @@
+import pyvolt as pv
+from pyvolt import components as comp
+
+circuit = pv.Circuit()
+# define the components of the circuit
+voltage_source = comp.VoltageSource(v=5) in circuit
+resistor = comp.Resistor(ohm=150) in circuit
+diode = comp.Diode(v_f=2, i_f=20e-3) in circuit
+# define the connections between components
+voltage_source.vplus >> resistor.n1
+resistor.n2 >> diode.anode
+diode.cathode >> voltage_source.vminus
+# set the circuit reference voltage
+circuit.gnd >> voltage_source.vminus
+# compile the circuit
+circuit.compile()
+# probe the circuit
+print("V before diode:", circuit.inspect_voltage(diode.anode))
+print("V after diode:", circuit.inspect_voltage(diode.cathode))
